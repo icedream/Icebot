a01ddefba5c4e2afec4dff4464be9565fc2ad034
Icedream <icedream2k9@die-optimisten.net>
2012-12-17 20:40:28 +0100

    Merged IrcCommand => IrcMessage.

--- Icebot/Irc/IrcMessage.cs
+++ Icebot/Irc/IrcMessage.cs
@@ -3,38 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Icebot.Irc
+namespace Icebot
 {
+    /// <summary>
+    /// Represents an IRC message.
+    /// </summary>
     public class IrcMessage
     {
-        public IrcMessageType Type { get; set; }
-        public string Target { get; set; }
-        public string Text { get; set; }
-        public bool IsPublic
+		public IrcMessage()
+		{
+            this.Command = null;
+            this.Parameters = new string[0];
+		}
+
+        public IrcMessage(string command)
         {
-            get
-            {
-                // TODO: Use server's information about channel name prefixes to check if target is a channel
-                return Target.Contains("#");
-            }
+            this.Command = command;
+            this.Parameters = new string[0];
         }
-    }
 
-    public class ReceivedIrcMessage : IrcMessage
-    {
-        public IrcStandardReply Reply { get; set; }
-        public IrcMessage Message { get; set; }
+		public IrcMessage(string command, params string[] parameters)
+		{
+			this.Command = command;
+			this.Parameters = parameters;
+		}
 
-        public ReceivedIrcMessage(IrcStandardReply reply)
+        public IrcMessage(string command, IEnumerable<string> parameters)
         {
-            Reply = reply;
+            this.Command = command;
+            this.Parameters = parameters.ToArray<string>();
+        }
 
-            var msg = new IrcMessage();
-            msg.Target = reply.Parameters[0];
-            msg.Type = reply.Command.Equals("PRIVMSG", StringComparison.OrdinalIgnoreCase) ? MessageType.PrivMsg : MessageType.Notice;
-            msg.Text = reply.Parameters[1];
+        private IrcMessage(string prefix, string command, IEnumerable<string> parameters)
+        {
+            this.Prefix = prefix;
+            this.Command = command;
+            this.Parameters = parameters.ToArray<string>();
+        }
 
-            Message = msg;
+        public string Prefix
+        { get; set; }
+
+		public string Command
+		{ get; set; }
+
+        public string[] Parameters
+        { get; set; }
+
+        public override string ToString()
+        {
+            if (Parameters != null && Parameters.Count() > 0)
+            {
+                string line = string.Format(
+                    "{2} {0} {1}",
+                    Command.ToUpper(),
+                    string.Join(" ", from p in Parameters select p != null ? (p.Contains(" ") ? ":" : "") + p : ""),
+                    Prefix
+                );
+                return line;
+            }
+            else
+                return Command.ToUpper();
+        }
+
+        public static IrcMessage Parse(string rawline)
+        {
+            int i = 0;
+            string prefix = string.Empty, cmd;
+            List<string> parameters = new List<string>();
+
+            // Check for a prefix
+            if (rawline.StartsWith(":"))
+            {
+                i++;
+                prefix = rawline.Substring(i, (i = rawline.IndexOf(' ', i) + 1) - 1);
+            }
+
+            // Parse command
+            cmd = rawline.Substring(i, (i = rawline.IndexOf(' ', i) + 1) - 1).ToUpper();
+
+            // Parse parameters
+            while (i < rawline.Length)
+            {
+                var next_space = rawline.IndexOf(' ', i);
+                var arg =
+                    rawline[i] == ':' // trailing
+                    ? rawline.Substring(i + 1)
+                    : next_space < 0
+                        ? rawline.Substring(i)
+                        : rawline.Substring(i, next_space)
+                    ;
+                parameters.Add(arg);
+                i += arg.Length + 1;
+            }
+
+            return new IrcMessage(prefix, cmd, parameters);
         }
     }
 }
